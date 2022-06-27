41e094a21035d0be4871fb859e2958257d3e5069
Duchstf <minhduc8199@gmail.com>
2022-06-26 19:25:09 -0500

    back up

---
+++ vh_combine/combine_scan/submit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os, sys
+import subprocess
+
+#Define the list of threshold
+ddb1_thresholds = 
+ddc2_thresholds = 
+
+#Process 10 points at a time
+n_chunk = 10
+
+counter = 0
+
+#Loop over all the threshold combinations
