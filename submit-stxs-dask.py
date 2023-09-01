e356021bb637f4cd18a0dfc0f49caaa4eb47937e
Jennet Dickinson <dickinson.jennet@gmail.com>
2023-09-01 11:10:25 -0500

    updated dask submit

--- submit-stxs-dask.py
+++ submit-stxs-dask.py
@@ -16,66 +16,78 @@ from dask_jobqueue import HTCondorCluster, SLURMCluster
 
 from datetime import datetime
 
-env_extra = [
-    f"export PYTHONPATH=$PYTHONPATH:{os.getcwd()}",
-]
+def main():
 
-cluster = LPCCondorCluster(
-    shared_temp_directory="/tmp",
-    transfer_input_files=["boostedhiggs"],
-    ship_env=True,
-    memory="10GB",
-)
+    env_extra = [
+        f"export PYTHONPATH=$PYTHONPATH:{os.getcwd()}",
+    ]
 
-if not os.path.isdir('outfiles-stxs/'):
-    os.mkdir('outfiles-stxs')
+    cluster = LPCCondorCluster(
+#        job_script_prologue=[
+#            "export DASK_DISTRIBUTED__WORKER__PROFILE__INTERVAL=1d",
+#            "export DASK_DISTRIBUTED__WORKER__PROFILE__CYCLE=2d",
+        #       ],
+        shared_temp_directory="/tmp",
+        transfer_input_files=["boostedhiggs"],
+        memory="8GB",
+        #        log_directory="/uscms/home/",
+    )
 
-cluster.adapt(minimum=1, maximum=500)
-with Client(cluster) as client:
+    if not os.path.isdir('outfiles-stxs/'):
+        os.mkdir('outfiles-stxs')
 
-    print(datetime.now())
-    print("Waiting for at least one worker...")  # noqa
-    client.wait_for_workers(1)
-    print(datetime.now())
+    cluster.adapt(minimum=1, maximum=500)
+    with Client(cluster) as client:
 
-    year = sys.argv[1]
+        print(datetime.now())
+        print("Waiting for at least one worker...")  # noqa
+        client.wait_for_workers(1)
+        print(datetime.now())
+        
+        year = sys.argv[1]
+        
+        with performance_report(filename="dask-report.html"):
+            
+            infiles = subprocess.getoutput("ls infiles/"+year+"_*.json").split()
+            
+            for this_file in infiles:
+                
+                index = this_file.split("_")[1].split(".json")[0]
+                outfile = 'outfiles-stxs/'+str(year)+'_dask_'+index+'.coffea'
+                
+                if "HToBB" not in index:
+                    continue
+                
+                if os.path.isfile(outfile):
+                    print("File " + outfile + " already exists. Skipping.")
+                    continue
+                else:
+                    print("Begin running " + outfile)
+                    print(datetime.now())
 
-    with performance_report(filename="dask-report.html"):
+                uproot.open.defaults["xrootd_handler"] = uproot.source.xrootd.MultithreadedXRootDSource
 
-        infiles = subprocess.getoutput("ls infiles/"+year+"_*.json").split()
+                p = VBFSTXSProcessor(year=year,jet_arbitration='ddb',ewkHcorr=True,systematics=True,skipJER=False)
+                args = {'savemetrics':True, 'schema':NanoAODSchema}
 
-        for this_file in infiles:
+                output = processor.run_uproot_job(
+                    this_file,
+                    treename="Events",
+                    processor_instance=p,
+                    executor=processor.dask_executor,
+                    executor_args={
+                        "client": client,
+                        "skipbadfiles": 1,
+                        "schema": processor.NanoAODSchema,
+                        "treereduction": 2,
+                    },
+                    chunksize=100000,
+                    #        maxchunks=args.max,
+                )
 
-            index = this_file.split("_")[1].split(".json")[0]
-            outfile = 'outfiles-stxs/'+str(year)+'_dask_'+index+'.coffea'
-
-            if os.path.isfile(outfile):
-                print("File " + outfile + " already exists. Skipping.")
-                continue
-            else:
-                print("Begin running " + outfile)
+                util.save(output, outfile)
+                print("saved " + outfile)
                 print(datetime.now())
 
-            uproot.open.defaults["xrootd_handler"] = uproot.source.xrootd.MultithreadedXRootDSource
-
-            p = VBFSTXSProcessor(year=year,jet_arbitration='ddb',ewkHcorr=True,systematics=True,skipJER=False)
-            args = {'savemetrics':True, 'schema':NanoAODSchema}
-
-            output = processor.run_uproot_job(
-                this_file,
-                treename="Events",
-                processor_instance=p,
-                executor=processor.dask_executor,
-                executor_args={
-                    "client": client,
-                    "skipbadfiles": 1,
-                    "schema": processor.NanoAODSchema,
-                    "treereduction": 2,
-                },
-                chunksize=100000,
-                #        maxchunks=args.max,
-            )
-
-            util.save(output, outfile)
-            print("saved " + outfile)
-            print(datetime.now())
+if __name__ == "__main__":
+    main()
